fe451a64bde96c6967405266cdc1d970a5d30d77
Rubalaine <kelvenizzy@gmail.com>
2021-03-27 17:47:38 +0200

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3da42559b0ae58225f19f6bf6ccda967'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'af2791b2-fc52-4ba0-986f-c66f9aae30ed'
+};
